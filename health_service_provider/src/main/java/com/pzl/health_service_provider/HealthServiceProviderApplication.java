02f8f5b172409bb1628b9aadf6bd9e17b6bb0e6d
YinLong <2804566988@qq.com>
2024-10-14 12:38:16 +0800

    项目环境搭建

---
+++ health_service_provider/src/main/java/com/pzl/health_service_provider/HealthServiceProviderApplication.java
@@ -0,0 +1,13 @@
+package com.pzl.health_service_provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthServiceProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthServiceProviderApplication.class, args);
+	}
+
+}
