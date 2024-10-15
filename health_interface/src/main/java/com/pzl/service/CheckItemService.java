02ce80ea7068d1d3d9f393d8ef73ed32feceb5ed
YinLong <2804566988@qq.com>
2024-10-15 14:01:16 +0800

    检查项管理-分页查询

--- health_interface/src/main/java/com/pzl/service/CheckItemService.java
+++ health_interface/src/main/java/com/pzl/service/CheckItemService.java
@@ -1,7 +1,9 @@
 package com.pzl.service;
 
+import com.pzl.entity.PageResult;
 import com.pzl.pojo.CheckItem;
 
 public interface CheckItemService {
     public void add(CheckItem checkItem);
+    public PageResult pageQuery(Integer currentPage, Integer pageSize, String queryString);
 }
--- health_service_provider/src/main/java/com/pzl/dao/CheckItemDao.java
+++ health_service_provider/src/main/java/com/pzl/dao/CheckItemDao.java
@@ -1,9 +1,13 @@
 package com.pzl.dao;
+import com.github.pagehelper.Page;
 import com.pzl.pojo.CheckItem;
+
 
 /**
  * 持久层Dao接口
  */
 public interface CheckItemDao {
     public void add(CheckItem checkItem);
+
+    public Page<CheckItem> selectByCondition(String queryString);
 }
