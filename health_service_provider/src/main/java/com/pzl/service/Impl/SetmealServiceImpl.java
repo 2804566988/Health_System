a7389e13e72b28c2c0461c78a3e643c55a4a8b03
YinLong <2804566988@qq.com>
2024-10-22 21:47:08 +0800

    使用Freemarker模版引擎完成页面静态化

--- health_service_provider/src/main/java/com/pzl/service/Impl/SetmealServiceImpl.java
+++ health_service_provider/src/main/java/com/pzl/service/Impl/SetmealServiceImpl.java
@@ -8,10 +8,15 @@ import com.pzl.dao.SetmealDao;
 import com.pzl.entity.PageResult;
 import com.pzl.pojo.Setmeal;
 import com.pzl.service.SetmealService;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
+import java.io.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,6 +33,12 @@ public class SetmealServiceImpl implements SetmealService {
     @Autowired
     private SetmealDao setmealDao;
 
+    @Autowired
+    private FreeMarkerConfigurer freeMarkerConfigurer;
+
+    @Value("${out_put_path}")//从属性文件读取输出目录的路径
+    private String outputpath;
+
     //新增套餐
     public void add(Setmeal setmeal, Integer[] checkgroupIds) {
         setmealDao.add(setmeal);
@@ -37,6 +48,8 @@ public class SetmealServiceImpl implements SetmealService {
         }
         //将图片名称保存到Redis
         redisTemplate.opsForSet().add(RedisConstant.SETMEAL_PIC_DB_RESOURCES, setmeal.getImg());
+        //生成静态页面
+        generateMobileStaticHtml();
     }
 
     //绑定套餐和检查组的多对多关系
@@ -67,6 +80,8 @@ public class SetmealServiceImpl implements SetmealService {
         setmealDao.edit(setmeal);
         //将图片名称保存到Redis
         redisTemplate.opsForSet().add(RedisConstant.SETMEAL_PIC_DB_RESOURCES, setmeal.getImg());
+        //生成静态页面
+        generateMobileStaticHtml();
     }
 
     //根据id查找套餐和检查组的关联关系
@@ -86,6 +101,62 @@ public class SetmealServiceImpl implements SetmealService {
         setmealDao.deleteAssociation(id);
         //根据id删除套餐基本信息
         setmealDao.deleteById(id);
+        //生成静态页面
+        generateMobileStaticHtml();
+    }
+
+    //生成静态页面
+    public void generateMobileStaticHtml() {
+        //准备模板文件中所需的数据
+        List<Setmeal> setmealList = this.findAll();
+        //生成套餐列表静态页面
+        generateMobileSetmealListHtml(setmealList);
+        //生成套餐详情静态页面（多个）
+        generateMobileSetmealDetailHtml(setmealList);
+    }
+
+    //生成套餐列表静态页面
+    public void generateMobileSetmealListHtml(List<Setmeal> setmealList) {
+        Map<String, Object> dataMap = new HashMap<String, Object>();
+        dataMap.put("setmealList", setmealList);
+        this.generateHtml("mobile_setmeal.ftl","m_setmeal.html",dataMap);
+    }
+
+    //生成套餐详情静态页面（多个）
+    public void generateMobileSetmealDetailHtml(List<Setmeal> setmealList) {
+        for (Setmeal setmeal : setmealList) {
+            Map<String, Object> dataMap = new HashMap<String, Object>();
+            dataMap.put("setmeal", this.findById(setmeal.getId()));
+            this.generateHtml("mobile_setmeal_detail.ftl",
+                    "setmeal_detail_"+setmeal.getId()+".html",
+                    dataMap);
+        }
+    }
+
+    public void generateHtml(String templateName,String htmlPageName,Map<String, Object> dataMap){
+        Configuration configuration = freeMarkerConfigurer.getConfiguration();
+        //处理空值
+        configuration.setClassicCompatible(true);
+        Writer out = null;
+        try {
+            // 加载模版文件
+            Template template = configuration.getTemplate(templateName);
+            // 生成数据
+            File docFile = new File(outputpath + "\\" + htmlPageName);
+            out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(docFile)));
+            // 输出文件
+            template.process(dataMap, out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != out) {
+                    out.flush();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
     }
 
     public List<Setmeal> findAll() {
